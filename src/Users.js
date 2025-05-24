07f6270d8c15add215739bb82eca9c25ca3ce7a1
Mohd Faisal Ansari <mohdfaisal.ansari@aircall.io>
2025-05-24 20:27:03 +0530

    first commit

---
+++ src/Users.js
@@ -0,0 +1,26 @@
+import { gql, useQuery } from '@apollo/client';
+
+const GET_USERS = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`;
+
+function Users() {
+  const { data, loading, error } = useQuery(GET_USERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <ul>
+      {data.users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+export default Users;
